4731cc54728e9a2075422f2b9987d005e26f5fa6
nsareen <nsareen@gmail.com>
2020-05-03 12:15:00 +0530

    sdfsdfsdf

---
+++ loops/test1.py
@@ -0,0 +1,17 @@
+print("hello world")
+print('I got a new tab')
+a1 = [1, 2, 3, 4]
+result = 3
+for r in a1:
+    if r == 1:
+        print(str(r) + ' This is the is first no. ')
+    else:
+        print(str(r) + ' This is the is not the first no. ')
+
+print('Papa I made my first program in tab 😀 ')
+
+list = [1, 3, 5, 7, 9]
+result = 3
+for r1 in list:
+    result = result + r1
+    print(result)
