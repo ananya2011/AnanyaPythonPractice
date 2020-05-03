9c9dd0620724a1ab6f886ddddedc882bf326554d
nsareen <nsareen@gmail.com>
2020-05-03 12:05:35 +0530

    practice files asdasd

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 message='whats your name'
 print(message)
 v1=input()
-v3=input('whats your age')
+v3=input('tell your age please')
 v5=input('your heigt is')
 v7=input('you cycle how many times')
 message='Good day '+ v1 + '!' + ' your age is '+ v3 +' your height is ' + v5 +' ft ' + 'you cycle '+ v7 + ' times '
